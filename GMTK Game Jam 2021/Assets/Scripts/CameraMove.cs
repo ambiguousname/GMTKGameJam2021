77e24fde78ff21d4662de2a217e7a192541def26
ambiguousname <tyly04@gmail.com>
2021-06-11 13:16:58 -0700

    Setting up player movement

--- GMTK Game Jam 2021/Assets/Scripts/CameraMove.cs
+++ GMTK Game Jam 2021/Assets/Scripts/CameraMove.cs
@@ -6,18 +6,27 @@ public class CameraMove : MonoBehaviour
 {
     public float maxSize = 2;
     public float minSize = 0.4f;
+    public Camera MainCamera;
     private float currentScale = 1;
     private Vector3 baseScale;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
         baseScale = this.transform.localScale;
+    }
+
+    public Vector3 GetCameraPos() {
+        return MainCamera.ScreenToWorldPoint(this.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1")) {
+            Cursor.visible = false;
+        }
         this.transform.position = Input.mousePosition + new Vector3(0, 0, -1);
         var scaleChange = Input.GetAxis("Mouse ScrollWheel");
         currentScale += scaleChange;
---
+++ GMTK Game Jam 2021/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CameraMove gameCamera;
+    public float playerSpeed = 1;
+    Rigidbody2D playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cameraPos = gameCamera.GetCameraPos();
+        var target = new Vector3(cameraPos.x, cameraPos.y);
+        target.Normalize();
+        if (Vector2.Distance(this.transform.position, new Vector3(cameraPos.x, cameraPos.y)) > 1.0f)
+        {
+            playerRigidbody.AddForce(target * playerSpeed);
+        }
+    }
+}
