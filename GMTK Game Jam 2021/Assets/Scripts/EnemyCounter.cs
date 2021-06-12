776a4226e0dcc95d8e2b2edce646f2f85d2f0f81
ambiguousname <tyly04@gmail.com>
2021-06-12 11:39:29 -0700

    Post processing, level tweaks

--- GMTK Game Jam 2021/Assets/Scripts/EnemyCounter.cs
+++ GMTK Game Jam 2021/Assets/Scripts/EnemyCounter.cs
@@ -8,16 +8,13 @@ public class EnemyCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!GameObject.Find("PauseManager").GetComponent<PauseManager>().isPaused)
+        var eCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        GetComponentInChildren<Text>().text = "" + eCount;
+        if (eCount <= 0)
         {
-            var eCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
-            GetComponentInChildren<Text>().text = "" + eCount;
-            if (eCount <= 0)
-            {
-                TakeCount.currentScene += 1;
-                TakeCount.currentTake = 0;
-                GameObject.Find("PauseManager").GetComponent<PauseManager>().GetWin();
-            }
+            TakeCount.currentScene += 1;
+            TakeCount.currentTake = 0;
+            GameObject.Find("PauseManager").GetComponent<PauseManager>().GetWin();
         }
     }
 }
